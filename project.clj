ef244262078ff195b0c1c783638c2bd2c22f805c
WojtAcht <wachtelik@gmail.com>
2022-09-22 12:22:18 +0200

    Update version - `0.1.5`

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tiny-auth "0.1.4"
+(defproject tiny-auth "0.1.5"
   :description "Authentication/authorization module."
   :url "https://github.com/spinneyio/tiny-auth"
   :dependencies [[org.clojure/clojure "1.10.3"]
