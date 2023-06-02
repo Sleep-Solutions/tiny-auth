673dff0183552ae622d875f3b20228ce4da53191
Marek Lipert <marek@spinney.io>
2023-06-02 10:45:51 +0200

    0.1.17

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tiny-auth "0.1.16"
+(defproject tiny-auth "0.1.17"
   :description "Authentication/authorization module."
   :url "https://github.com/spinneyio/tiny-auth"
   :dependencies [[org.clojure/clojure "1.10.3"]
